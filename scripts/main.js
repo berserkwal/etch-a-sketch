ecb54b801c143eb36c9d9ecfa8d53c1b6a4a11d4
berserkwal <kharkwalvikas@gmail.com>
2021-05-22 13:38:45 +0530

    did most of the logic and added all the required event listeners including for shortcut keys

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+// dynamic viewport height that I use to fix 100vh pages across devices
+
 let viewportHeight = window.innerHeight / 100 + "px";
 document.documentElement.style.setProperty("--vh", viewportHeight);
 
@@ -6,44 +8,28 @@ window.addEventListener("resize", () => {
 	document.documentElement.style.setProperty("--vh", viewportHeight);
 });
 
+//variable declarations below
+
 const canvas = document.querySelector(".canvas");
 const brushButton = document.querySelector(".toolbar-button.brush");
 const eraserButton = document.querySelector(".toolbar-button.eraser");
 const clearButton = document.querySelector(".toolbar-button.clear");
 const resizeButton = document.querySelector(".toolbar-button.resize");
 const colorPicker = document.querySelector(".toolbar-button.color-picker");
+const modalOverlay = document.querySelector(".resize-modal");
+const modalCloseButton = document.querySelector("#close-modal");
+const gridSizeRange = document.querySelector(".size-selection");
+const gridSizeSubmit = document.querySelector(".selection");
+const gridDim = document.querySelectorAll(".grid-size>span");
 
-// let method = brush;
 let color = "black";
-let dimension = 12;
+let dimension = 8;
 
 let isDrawable = false;
 
 resize(dimension);
 
-colorPicker.addEventListener("change", (e) => {
-	color = e.target.value;
-});
-
-brushButton.addEventListener("click", () => {
-	// brush();
-	color = colorPicker.value;
-	brushButton.classList.add("selected");
-	eraserButton.classList.remove("selected");
-});
-
-eraserButton.addEventListener("click", () => {
-	// eraser();
-	color = "white";
-	// method = eraser;
-	eraserButton.classList.add("selected");
-	brushButton.classList.remove("selected");
-});
-
-clearButton.addEventListener("click", () => {
-	removePixels();
-	generateGrid();
-});
+// functions below
 
 function resize(newDimension) {
 	removePixels();
@@ -75,16 +61,88 @@ function removePixels() {
 	pixelArray.forEach((node) => {
 		node.remove();
 	});
+
+	isDrawable = false;
 }
 
-function pixelEvents() {
+function setPixelEvents() {
 	const pixels = document.querySelectorAll(".pixel");
 	pixels.forEach((pixel) => pixel.addEventListener("mousemove", draw));
 }
 
+function openModal() {
+	modalOverlay.classList.remove("no-visibility");
+	resizeButton.classList.add("selected");
+}
+function closeModal() {
+	modalOverlay.classList.add("no-visibility");
+	resizeButton.classList.remove("selected");
+}
+
+function brush() {
+	color = colorPicker.value;
+	brushButton.classList.add("selected");
+	eraserButton.classList.remove("selected");
+}
+
+function erase() {
+	color = "white";
+	eraserButton.classList.add("selected");
+	brushButton.classList.remove("selected");
+}
+
+// event-listeners below
+
+gridSizeRange.addEventListener("input", () => {
+	gridDim.forEach((item) => {
+		item.innerText = gridSizeRange.value;
+	});
+});
+
+gridSizeSubmit.addEventListener("click", () => {
+	dimension = gridSizeRange.value;
+	closeModal();
+	resize(dimension);
+});
+
+modalCloseButton.addEventListener("click", closeModal);
+
 canvas.addEventListener("click", () => {
 	isDrawable = !isDrawable;
 	const pixels = document.querySelectorAll(".pixel");
-	if (isDrawable) pixelEvents();
+	if (isDrawable) setPixelEvents();
 	else pixels.forEach((pixel) => pixel.removeEventListener("mousemove", draw));
+});
+
+colorPicker.addEventListener("change", (e) => {
+	color = e.target.value;
+});
+
+brushButton.addEventListener("click", brush);
+
+eraserButton.addEventListener("click", erase);
+
+resizeButton.addEventListener("click", openModal);
+
+clearButton.addEventListener("click", () => {
+	removePixels();
+	generateGrid();
+});
+
+window.addEventListener("keydown", (e) => {
+	switch (e.code) {
+		case "KeyB":
+			brush();
+			break;
+		case "KeyE":
+			erase();
+			break;
+		case "KeyC":
+			removePixels();
+			generateGrid();
+			break;
+		case "KeyR":
+			openModal();
+			break;
+	}
 });
